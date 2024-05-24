43273ab4ababa10b367bb3f14c870a58cb560c59
paulineblmms <blummoysepauline@gmail.com>
2024-05-24 18:32:19 +0700

    Update models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_suspended = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
