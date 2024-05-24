a926543d949d810be55341bcb6d7e8e9177e9b7d
paulineblmms <blummoysepauline@gmail.com>
2024-05-24 18:46:23 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,3 +6,9 @@ app_name = 'main'
 urlpatterns = [
     path('report-list', report_list, name='report_list'),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('mood_tracking/', include('mood_tracking.urls')),
+    # Include other app URLs here as needed
+]
