ef0fac9a3efa6a194c3497ac2ed9e69143701c46
paulineblmms <blummoysepauline@gmail.com>
2024-05-24 18:25:58 +0700

    Update views.py

--- mood_tracking/views.py
+++ mood_tracking/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
-from main.forms import MoodForm
-from main.models import Mood
+from .models import Mood
+from .forms import MoodForm
 
-def mood_tracking(request):
+def mood_list(request):
+    moods = Mood.objects.all()
+    return render(request, 'mood/mood_list.html', {'moods': moods})
+
+def mood_create(request):
     if request.method == 'POST':
-        form = MoodForm(request.POST, user=request.user)
+        form = MoodForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('mood_tracking:mood_tracking')
+            return redirect('mood_list')
     else:
         form = MoodForm()
+    return render(request, 'mood/mood_form.html', {'form': form})
 
-    moods = Mood.objects.filter(user=request.user)
-    context = {'form': form, 'moods': moods}
-    return render(request, 'mood_tracking.html', context)
+def mood_links(request, mood_name):
+    mood_links = {
+        "happy": "https://www.happier.com",
+        "sad": "https://www.verywellmind.com/how-to-cope-with-sadness-3145105",
+        "anxious": "https://www.helpguide.org/articles/anxiety/anxiety-disorders-and-anxiety-attacks.htm",
+        "angry": "https://www.apa.org/topics/anger/control",
+        "relaxed": "https://www.mindful.org/meditation/mindfulness-getting-started/",
+        # Add more moods and corresponding links as needed
+    }
+    link = mood_links.get(mood_name.lower(), "No link available for this mood")
+    return render(request, 'mood/mood_links.html', {'mood': mood_name, 'link': link})
