a6db619658b15af419d1aa3187fc1b1b0379318b
paulineblmms <blummoysepauline@gmail.com>
2024-05-24 18:35:17 +0700

    Update apps.py

--- mood_tracking/apps.py
+++ mood_tracking/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class MoodTrackingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mood_tracking'
+
+class AuthenticationConfig(AppConfig):
+    name = 'authentication'
+
+    def ready(self):
+        import authentication.signals
