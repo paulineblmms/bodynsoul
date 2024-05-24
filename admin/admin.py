18dda5cf00a832114de4eba1dcee251d570daabe
paulineblmms <blummoysepauline@gmail.com>
2024-05-24 18:38:23 +0700

    Update admin.py

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from authentication.models import UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (UserProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
